6d40f8c811a0dc1646e0dd02bdf5a81676980179
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2024-11-01 08:49:29 +0530

    Update heap_by_array.cpp

--- heap_by_array.cpp
+++ heap_by_array.cpp
@@ -11,7 +11,7 @@ class heap{
     heap(){
         arr[0]=-1;
         size=0;
-    }
+    } 
 
     //for insert
     void insert(int value){
@@ -36,6 +36,46 @@ class heap{
         }
     }
     
+    //for delete
+    void deletefromHeap(){
+        //empty heap
+        if(size==0){
+            cout<<"nothing to delete"<<endl;
+            return;
+        }
+        //step1:put the last element into first index
+        arr[1]=arr[size];
+        //step2:remove last element
+        size--; 
+        
+        //step3:take root node to its correct position after swapping
+        int i =1;
+        while(i<size){
+            int leftIndex=2*i;
+            int rightIndex=2*i+1;
+            
+            if(leftIndex < size && arr[i] < arr[leftIndex]){
+                int temp=arr[i];
+                arr[i]=arr[leftIndex];
+                arr[leftIndex]=temp;
+                
+                //update to continue the heapify
+                i = leftIndex;
+            }
+            else if(rightIndex < size && arr[i] < arr[rightIndex]){
+                int temp=arr[i];
+                arr[i]=arr[rightIndex];
+                arr[rightIndex]=temp;
+                
+                //update to continue the heapify
+                i = rightIndex;
+            }
+            else{
+                return;
+            }
+        }   
+    }
+    
     //for print
     void print(){
         for(int i = 1; i <= size; i++){
@@ -52,6 +92,8 @@ int main() {
     h.insert(53);
     h.insert(52);
     h.insert(54);
+    h.print();
+    h.deletefromHeap();
     h.print();
 
     return 0;
